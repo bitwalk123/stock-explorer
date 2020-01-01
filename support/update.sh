3a894eed102dfed94c5e4e9cbc9595cd9ec1f033
Fuhito Suguri <bitwalk@gmail.com>
2020-01-01 10:57:06 +0900

    modify support/update.R to update sqlite database in the git repositry automatically.

--- support/update.sh
+++ support/update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 R CMD BATCH --vanilla --slave /home/bitwalk/shiny-app/stock-explorer/support/update.R
+cd /home/bitwalk/shiny-app/stock-explorer/; git add stock-explorer.sqlite3; git commit -m "daily update"; git push
