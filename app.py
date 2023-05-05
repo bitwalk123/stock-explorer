7aded0b1dce12f8d6290dcc44efc8ca94732659c
Keiichi Takahashi <bitwalk@gmail.com>
2023-05-06 07:20:27 +0900

    update

--- app.py
+++ app.py
@@ -23,23 +23,24 @@ class StockExplorer(QMainWindow):
         super().__init__()
         self.setWindowTitle('Stock Explorer')
 
-        # OS depended
+        # OS 別に ini ファイルを設定
         if platform.system() == 'Windows':
             file_config = 'stock_explorer.ini'
         else:
             file_config = '.stock_explorer'
 
-        # ini file
+        # ini ファイル（フルパス）
         self.file_ini = os.path.join(expanduser('~'), file_config)
         print(self.file_ini)
+
         self.init_ui()
 
     def init_ui(self):
-        # Create pyqt toolbar
+        # ツールバー
         toolbar = QToolBar()
         self.addToolBar(toolbar)
 
-        # spacer
+        # 余白のスペーサ
         spacer = QWidget()
         spacer.setSizePolicy(
             QSizePolicy.Policy.Expanding,
@@ -57,18 +58,15 @@ class StockExplorer(QMainWindow):
         but_conf.clicked.connect(self.show_conf_dialog)
         toolbar.addWidget(but_conf)
 
-        # Exit application
-        but_exit = QToolButton()
-        but_exit.setText('Exit')
-        but_exit.setToolTip('アプリケーションの終了')
-        name = 'SP_BrowserStop'
-        icon_exit = get_standard_icon(self, name)
-        but_exit.setIcon(icon_exit)
-        toolbar.addWidget(but_exit)
-
     def show_conf_dialog(self):
         dlg = DlgConfig(parent=self)
         dlg.show()
+
+    def closeEvent(self, event):
+        """Close event when user click X button.
+        """
+        print('アプリケーションを終了します。')
+        event.accept()  # let the window close
 
 
 def main():
--- modules/config_dialog.py
+++ modules/config_dialog.py
@@ -1,14 +1,14 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
-    QMainWindow,
-    QTabWidget,
+    QDialog,
+    QTabWidget, QVBoxLayout, QDialogButtonBox,
 )
 
 from functions.get_standard_ison import get_standard_icon
 from modules.panel_db import PanelDB
 
 
-class DlgConfig(QMainWindow):
+class DlgConfig(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowFlag(Qt.WindowStaysOnTopHint, True)
@@ -20,10 +20,16 @@ class DlgConfig(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
         tab = QTabWidget()
-        self.setCentralWidget(tab)
+        layout.addWidget(tab)
 
         panel_db = PanelDB()
         tab.addTab(panel_db, panel_db.getTabLabel())
 
-
+        dlg_button = QDialogButtonBox.StandardButton.Ok
+        bbox = QDialogButtonBox(dlg_button)
+        bbox.accepted.connect(self.accept)
+        layout.addWidget(bbox)
