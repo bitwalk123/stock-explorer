ecd19ac85d620cf2f0c59655dce126f7af5570ed
Fuhito Suguri <bitwalk@gmail.com>
2023-06-21 05:51:20 +0900

    update

--- snippet/yfinance03.py
+++ snippet/yfinance03.py
@@ -10,10 +10,10 @@ import pandas_datareader.data as web
 start = dt.date(2019, 1, 1)
 end = dt.date(2020, 1, 1)
 
-# df = web.DataReader('9984.T', 'yahoo', start, end)
+df = web.DataReader('9984.T', 'yahoo', start, end)
 # df = web.DataReader('9432.T', "yahoo", start, end)
-symbol = 'WIKI/AAPL'  # or 'AAPL.US'
-
-#df = web.DataReader(symbol, 'quandl', '2015-01-01', '2015-01-05')
-df = web.DataReader('GE', 'yahoo', start='2019-09-10', end='2019-10-09')
+# symbol = 'WIKI/AAPL'  # or 'AAPL.US'
+# df = web.DataReader(symbol, 'quandl', '2015-01-01', '2015-01-05')
+# df = web.DataReader('GE', 'yahoo', start='2019-09-10', end='2019-10-09')
+# df = web.DataReader('GOOG', 'yahoo-actions', start, end)
 print(df)
