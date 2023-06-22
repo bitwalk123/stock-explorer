766097fbc1a8d35fddff13dc3c6d45bdf0e09c95
Fuhito Suguri <bitwalk@gmail.com>
2023-06-23 05:44:31 +0900

    update

--- snippet/list_ticker.py
+++ snippet/list_ticker.py
@@ -1,13 +1,23 @@
+import os
+import datetime as dt
+import yfinance as yf
+
 from PySide6.QtSql import QSqlQuery
 
 from functions.resources import get_connection
 
+start = dt.date(2010, 1, 1)
+end = dt.date.today()
+
 con = get_connection()
 if con.open():
-    sql = 'select コード, 銘柄名 from ticker;'
+    sql = 'select id_ticker, コード, 銘柄名 from ticker;'
     query = QSqlQuery(sql)
     while query.next():
-        code = '%d.T' % query.value(0)
-        desc = str(query.value(1))
-        print(code, desc)
+        id = query.value(0)
+        code = '%d.T' % query.value(1)
+        desc = str(query.value(2))
+        df = yf.download(code, start, end)
+        filename = '%s.csv' % os.path.join('data', code)
+        df.to_csv(filename)
     con.close()
