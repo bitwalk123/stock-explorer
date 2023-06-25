a5c54426b03b1f7cf71de7865704af3c2a5815a9
Fuhito Suguri <bitwalk@gmail.com>
2023-06-25 09:59:21 +0900

    update

--- snippet/load_ticker_file_component_selection.py
+++ snippet/load_ticker_file_component_selection.py
@@ -29,11 +29,14 @@ for code in list_code:
     list_close.append(df['Close'].rename(code))
 
 df_close = pd.concat(list_close, axis=1)
-columns_result = ['Components', 'R2 calib', 'R2 CV', 'MSE calib', 'MSE CV']
+print(df_close)
+
 # empty dataframe
+columns_result = ['Components', 'R2 calib', 'R2 CV', 'MSE calib', 'MSE CV']
 df_result = pd.DataFrame(columns=columns_result)
+
 # loop for each ticker
-for code_target in list_code[0:2]:
+for code_target in list_code:
     filename = '%s.csv' % os.path.join('data', '%d.T' % code_target)
     series_open_target = pd.read_csv(filename, index_col=0)['Open']
     label_code_target = '%d Open' % code_target
@@ -42,7 +45,7 @@ for code_target in list_code[0:2]:
     df_open = series_open_target[1:len(series_open_target)]
     df_all[label_code_target] = df_open.values
     df_all.dropna(how='any', axis=1, inplace=True)
-    print(df_all)
+    # print(df_all)
 
     X = df_all.iloc[:, 0:len(df_all.columns) - 1].values
     y = df_all[label_code_target].values
