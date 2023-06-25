77ad76487749044b3f838f2c3774748d7591dd20
Fuhito Suguri <bitwalk@gmail.com>
2023-06-25 16:06:05 +0900

    update

---
+++ get_ticker_data_csv.py
@@ -0,0 +1,25 @@
+import os
+import datetime as dt
+import yfinance as yf
+
+from PySide6.QtSql import QSqlQuery
+
+from functions.resources import get_connection
+
+start = dt.date(2000, 1, 1)
+#start = dt.date(2022, 1, 1)
+end = dt.date.today()
+
+con = get_connection()
+if con.open():
+    sql = 'select id_ticker, コード, 銘柄名 from ticker;'
+    query = QSqlQuery(sql)
+    while query.next():
+        id = query.value(0)
+        code = '%d.T' % query.value(1)
+        print(code)
+        desc = str(query.value(2))
+        df = yf.download(code, start, end)
+        filename = '%s.csv' % os.path.join('data', code)
+        df.to_csv(filename)
+    con.close()
---
+++ load_ticker_file_component_selection.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+import pandas as pd
+from PySide6.QtSql import QSqlQuery
+
+from functions.get_csv_name import get_csv_name
+from functions.prediction import (
+    search_minimal_component_number,
+    minimal_scores,
+)
+from functions.resources import get_connection
+
+con = get_connection()
+if not con.open():
+    sys.exit()
+
+list_code = list()
+sql = 'select コード from ticker;'
+query = QSqlQuery(sql)
+while query.next():
+    list_code.append(query.value(0))
+con.close()
+
+list_close = list()
+for code in list_code:
+    filename = '%s.csv' % os.path.join('data', '%d.T' % code)
+    df = pd.read_csv(filename, index_col=0)
+    list_close.append(df['Close'].rename(code))
+
+df_close = pd.concat(list_close, axis=1)
+print(df_close)
+
+# empty dataframe
+columns_result = ['Components', 'R2 calib', 'R2 CV', 'MSE calib', 'MSE CV']
+df_result = pd.DataFrame(columns=columns_result)
+
+# loop for each ticker
+for code_target in list_code:
+    filename = '%s.csv' % os.path.join('data', '%d.T' % code_target)
+    series_open_target = pd.read_csv(filename, index_col=0)['Open']
+    label_code_target = '%d Open' % code_target
+
+    df_all = df_close.iloc[0:len(df_close) - 1, :].copy()
+    df_open = series_open_target[1:len(series_open_target)]
+    df_all[label_code_target] = df_open.values
+    df_all.dropna(how='any', axis=1, inplace=True)
+
+    X = df_all.iloc[:, 0:len(df_all.columns) - 1].values
+    y = df_all[label_code_target].values
+    print(X.shape)
+    print(y.shape)
+
+    mse_min = search_minimal_component_number(X, y)
+
+    n_comp = mse_min + 1
+    result = minimal_scores(X, y, n_comp)
+    series_target = pd.Series(
+        data=[n_comp, result['R2 calib'], result['R2 CV'], result['MSE calib'], result['MSE CV']],
+        index=columns_result,
+        name=code_target
+    )
+    df_result.loc[code_target] = series_target
+    print(df_result)
+    # save result every time
+    df_result.to_csv('result_pls.csv')
