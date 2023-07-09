3306fbb9f15f7e64924ab43d5e77e90775c501cf
Fuhito Suguri <bitwalk@gmail.com>
2023-07-09 16:07:26 +0900

    update

--- set_init_db_trade.py
+++ set_init_db_trade.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 from PySide6.QtSql import QSqlQuery
 
 from functions.resources import get_connection
@@ -12,5 +12,12 @@ if con.open():
         id = query.value(0)
         code = '%d.T' % query.value(1)
         filename = '%s.csv' % os.path.join('data', code)
-        print(filename)
+        if os.path.isfile(filename):
+            df = pd.read_csv(filename)
+            if len(df) == 0:
+                continue
+            print(id, filename)
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['Date'] = [int(df['Date'][r].timestamp()) for r in range(len(df))]
+            print(df.head())
     con.close()
