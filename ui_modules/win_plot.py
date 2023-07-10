a4113763dca9a93f2f7957c2e0f6437503b2e7f8
Fuhito Suguri <bitwalk@gmail.com>
2023-07-11 06:24:09 +0900

    update

--- ui_modules/win_plot.py
+++ ui_modules/win_plot.py
@@ -1,12 +1,18 @@
-from PySide6.QtWidgets import QWidget, QSizePolicy, QMainWindow
-from matplotlib_inline.backend_inline import FigureCanvas
+# Reference
+# https://www.pythonguis.com/tutorials/pyside6-plotting-matplotlib/
+import matplotlib
+
+matplotlib.use('Qt5Agg')
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
 
 
-class WinPlot(QMainWindow):
+class WinPlot(FigureCanvasQTAgg):
+
     def __init__(self):
-        super().__init__()
-        self.setContentsMargins(0, 0, 0, 0)
-        #self.setSizePolicy(
-        #    QSizePolicy.Policy.Expanding,
-        #    QSizePolicy.Policy.Expanding
-        #)
+        # fig = Figure(figsize=(width, height), dpi=dpi)
+        fig = Figure()
+        self.axes = fig.add_subplot(111)
+        super().__init__(fig)
+        # self.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
