a074fd5a34bbcfb40dfff8967ab8020358c07d25
Fuhito Suguri <bitwalk@gmail.com>
2023-09-14 05:42:03 +0900

    update

--- sim_price_001.py
+++ sim_price_001.py
@@ -40,7 +40,7 @@ def main():
     end_dt = dt.datetime.strptime(end_str, '%Y-%m-%d')
 
     # Loop condition
-    duration = 210 * day
+    duration = 240 * day
     origin = end = int(dt.datetime.timestamp(end_dt)) + tz_delta
 
     while end < origin + duration:
