62f6f24d9f58d30e47cc61e4e543f4eea67e88fe
Keiichi Takahashi <bitwalk@gmail.com>
2023-09-17 16:26:18 +0900

    update

--- functions/get_info_ticker.py
+++ functions/get_info_ticker.py
@@ -1,7 +1,20 @@
+import os
+import pickle
+
 import yfinance as yf
 
 
 def get_info_ticker(code) -> dict:
     ticker = '%s.T' % code
-    ticker_info = yf.Ticker(ticker)
-    return ticker_info.info
+    dict_info_ticker = None
+    pkl_dict_info_ticker = 'pool/dict_info_%s.pkl' % ticker
+    if os.path.isfile(pkl_dict_info_ticker):
+        with open(pkl_dict_info_ticker, 'rb') as f:
+            dict_info_ticker = pickle.load(f)
+    else:
+        ticker_info = yf.Ticker(ticker)
+        dict_info_ticker = ticker_info.info
+        with open(pkl_dict_info_ticker, 'wb') as f:
+            pickle.dump(dict_info_ticker, f)
+
+    return dict_info_ticker
