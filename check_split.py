17ff77127d8185b6c3d2dfa6af9e2240bb10679b
Keiichi Takahashi <bitwalk@gmail.com>
2023-09-18 10:20:58 +0900

    update

--- check_split.py
+++ check_split.py
@@ -1,46 +1,9 @@
 import datetime as dt
-import sys
 
-from PySide6.QtSql import QSqlQuery
-
-from database.sqls import (
-    get_sql_select_date_open_from_trade_with_id_code_start_end,
-)
 from functions.conv_timestamp2date import conv_timestamp2date
 from functions.get_dict_code import get_dict_code
+from functions.preprocess import preProcess
 from functions.resources import get_connection
-
-
-class preProcess():
-    factor_split = 1.5
-
-    def __init__(self, id_code: int, start:int, end:int):
-        self.id_code = id_code
-        self.start = start
-        self.end = end
-        self.date = 0
-        self.price_open = 0
-        self.price_open_pre = -1
-
-    def is_split(self) -> bool:
-        flag_date = self.price_open_pre > 0
-        flag_upper = self.price_open_pre / self.factor_split > self.price_open
-        flag_lower = self.price_open_pre * self.factor_split < self.price_open
-        return flag_date and (flag_upper or flag_lower)
-
-    def check(self) -> bool:
-        sql = get_sql_select_date_open_from_trade_with_id_code_start_end(
-            self.id_code, self.start, self.end
-        )
-        query = QSqlQuery(sql)
-        while query.next():
-            self.date = query.value(0)
-            self.price_open = query.value(1)
-            if self.is_split():
-                return True
-            self.price_open_pre = self.price_open
-
-        return False
 
 
 def main():
@@ -56,7 +19,6 @@ def main():
     num_total = 0
     con = get_connection()
     if con.open():
-        print('success to open db.')
         # prepare dictionary for id_code and code
         dict_code: dict = get_dict_code()
 
