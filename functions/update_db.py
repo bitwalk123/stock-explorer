11af1d848eea712cae5f1f9fcab5488799b3fa2a
Fuhito Suguri <bitwalk@gmail.com>
2023-10-20 06:11:55 +0900

    update

--- functions/update_db.py
+++ functions/update_db.py
@@ -12,19 +12,24 @@ from functions.conv_timestamp2date import conv_timestamp2date_next
 from functions.resources import get_connection
 
 
-def update_ticker(end):
+def update_ticker(end, queries: list):
+    query1: QSqlQuery = queries[0]
+    query2: QSqlQuery = queries[1]
+    query3: QSqlQuery = queries[2]
+    query4: QSqlQuery = queries[3]
+
     con = get_connection()
     if con.open():
         # get list of id_code, code from the ticker table
         sql1 = get_sql_select_id_code_code_from_ticker()
-        query1 = QSqlQuery(sql1)
+        query1.exec(sql1)
         while query1.next():
             id_code = query1.value(0)
             code = '%d.T' % query1.value(1)
 
             # get latest date of the trade table with specified id_code
             sql2 = get_sql_select_max_date_from_trade_with_id_code(id_code)
-            query2 = QSqlQuery(sql2)
+            query2.exec(sql2)
             while query2.next():
                 date_max = query2.value(0)
                 if type(date_max) is not int:
@@ -44,7 +49,7 @@ def update_ticker(end):
 
                     # get id_trade from the trade table with specified date and id_code
                     sql3 = get_sql_select_id_trade_from_trade_with_date_id_code(date, id_code)
-                    query3 = QSqlQuery(sql3)
+                    query3.exec(sql3)
                     if query3.next():
                         # if data exists, update the trade table
                         id_trade = query3.value(0)
@@ -53,6 +58,5 @@ def update_ticker(end):
                         # if not, append data to the trade table
                         sql4 = get_sql_insert_into_trade_values(id_code, series)
                     # execute query
-                    query4 = QSqlQuery()
                     query4.exec(sql4)
         con.close()
--- update_ticker_data_db.py
+++ update_ticker_data_db.py
@@ -1,7 +1,10 @@
 import datetime as dt
 
+from PySide6.QtSql import QSqlQuery
+
 from functions.update_db import update_ticker
 
 if __name__ == '__main__':
     end = dt.date.today()
-    update_ticker(end)
+    queries = [QSqlQuery() for k in range(4)]
+    update_ticker(end, queries)
