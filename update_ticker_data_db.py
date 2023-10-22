6cee80f865c8040af08922fb4e08021082d96f71
Fuhito Suguri <bitwalk@gmail.com>
2023-10-22 18:53:11 +0900

    update

--- update_ticker_data_db.py
+++ update_ticker_data_db.py
@@ -1,10 +1,14 @@
 import datetime as dt
+import time
 
 from PySide6.QtSql import QSqlQuery
 
+from functions.get_elapsed import get_elapsed
 from functions.update_db import update_ticker
 
 if __name__ == '__main__':
+    time_start = time.time()
     end = dt.date.today()
     queries = [QSqlQuery() for k in range(4)]
     update_ticker(end, queries)
+    print('elapsed %.3f sec' % get_elapsed(time_start))
