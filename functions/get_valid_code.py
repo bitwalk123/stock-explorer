0bb06405c26918bb4439b2240214fd2370952f40
Fuhito Suguri <bitwalk@gmail.com>
2023-10-22 19:27:18 +0900

    update

---
+++ functions/get_valid_code.py
@@ -0,0 +1,54 @@
+from functions.app_enum import PreProcessExcluded
+from functions.conv_timestamp2date import conv_timestamp2date
+from functions.get_dict_code import get_dict_code
+from functions.preprocess import PreProcess
+
+
+def get_valid_code(start: int, end: int) -> tuple:
+    # valid list of id_code
+    list_valid_id_code = list()
+    list_target_id_code = list()
+    num_total = 0
+    # prepare dictionary for id_code and code
+    dict_code: dict = get_dict_code()
+    for id_code in dict_code.keys():
+        code = dict_code[id_code]
+        prep = PreProcess(id_code, start, end)
+
+        if prep.IsExclude():
+            num_total += 1
+            print('%d.T is excluded : ' % code, end='')
+
+            if prep.FLAG_EXCLUDE == PreProcessExcluded.EMPTY:
+                print('No Data!')
+            elif prep.FLAG_EXCLUDE == PreProcessExcluded.VOLUME:
+                print(
+                    'Volume(Median) =',
+                    prep.volume_median,
+                )
+            elif prep.FLAG_EXCLUDE == PreProcessExcluded.SPLIT:
+                print(
+                    'Volume(Median) =',
+                    prep.volume_median,
+                    'Split',
+                    conv_timestamp2date(prep.date),
+                    prep.price_open_pre, '>>',
+                    prep.price_open, ':',
+                    prep.price_open - prep.price_open_pre,
+                )
+            else:
+                print('Unknown')
+        else:
+            list_valid_id_code.append(id_code)
+            if prep.IsTarget():
+                print(
+                    '%d.T is target: Open(Median) = %.1f JPY, Volume(Median) = %d' % (
+                        code,
+                        prep.open_median,
+                        prep.volume_median,
+                    )
+                )
+                list_target_id_code.append(id_code)
+
+    print('excluded %d out of %d tickers' % (num_total, len(dict_code.keys())))
+    return list_valid_id_code, list_target_id_code
--- get_valid_data.py
+++ get_valid_data.py
@@ -1,11 +1,8 @@
 import datetime as dt
 import time
 
-from functions.app_enum import PreProcessExcluded
-from functions.conv_timestamp2date import conv_timestamp2date
-from functions.get_dict_code import get_dict_code
 from functions.get_elapsed import get_elapsed
-from functions.preprocess import PreProcess
+from functions.get_valid_code import get_valid_code
 from functions.resources import get_connection
 
 
@@ -23,62 +20,16 @@ def main():
         dt.datetime.fromtimestamp(end)
     )
 
-    # valid list of id_code
-    list_valid_id_code = list()
-    list_target_id_code = list()
-    dict_code = dict()
-    num_total = 0
-
     con = get_connection()
     if con.open():
-        # prepare dictionary for id_code and code
-        dict_code: dict = get_dict_code()
-        for id_code in dict_code.keys():
-            code = dict_code[id_code]
-            prep = PreProcess(id_code, start, end)
-
-            if prep.IsExclude():
-                num_total += 1
-                print('%d.T is excluded : ' % code, end='')
-
-                if prep.FLAG_EXCLUDE == PreProcessExcluded.EMPTY:
-                    print('No Data!')
-                elif prep.FLAG_EXCLUDE == PreProcessExcluded.VOLUME:
-                    print(
-                        'Volume(Median) =',
-                        prep.volume_median,
-                    )
-                elif prep.FLAG_EXCLUDE == PreProcessExcluded.SPLIT:
-                    print(
-                        'Volume(Median) =',
-                        prep.volume_median,
-                        'Split',
-                        conv_timestamp2date(prep.date),
-                        prep.price_open_pre, '>>',
-                        prep.price_open, ':',
-                        prep.price_open - prep.price_open_pre,
-                    )
-                else:
-                    print('Unknown')
-            else:
-                list_valid_id_code.append(id_code)
-                if prep.IsTarget():
-                    print(
-                        '%d.T is target: Open(Median) = %.1f JPY, Volume(Median) = %d' % (
-                            code,
-                            prep.open_median,
-                            prep.volume_median,
-                        )
-                    )
-                    list_target_id_code.append(id_code)
+        # get list of valid code and target
+        list_valid_id_code, list_target_id_code = get_valid_code(start, end)
+        print('number of valid id_code : %d' % len(list_valid_id_code))
+        print('number of target id_code : %d' % len(list_target_id_code))
 
         con.close()
     else:
         print('fail to open db.')
-
-    print('excluded %d out of %d tickers' % (num_total, len(dict_code.keys())))
-    print('number of valid id_code : %d' % len(list_valid_id_code))
-    print('number of target id_code : %d' % len(list_target_id_code))
 
 
 if __name__ == "__main__":
