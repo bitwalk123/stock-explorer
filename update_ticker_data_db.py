a17ec8564614fa6281530a1117de4ce914f8a301
Fuhito Suguri <bitwalk@gmail.com>
2023-10-24 15:45:26 +0900

    update

--- update_ticker_data_db.py
+++ update_ticker_data_db.py
@@ -9,9 +9,7 @@ from functions.update_db import (
 
 if __name__ == '__main__':
     time_start = time.time()
-    # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
     end = dt.date.today()
     update_ticker(end)
-    # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
     update_prediction()
     print('elapsed %.3f sec' % get_elapsed(time_start))
