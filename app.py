a06d1b77524a48d3dde742c108b10e2717ed1d99
Fuhito Suguri <bitwalk@gmail.com>
2023-10-24 15:46:02 +0900

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class StockExplorer(QMainWindow):
     """Main class for this application
     """
     __version__ = '0.1.0'
-    __build__ = '20231010'
+    __build__ = '20231024'
 
     def __init__(self):
         super().__init__()
