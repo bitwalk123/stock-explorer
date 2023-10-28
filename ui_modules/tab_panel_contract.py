58d4c31d591a75a7ee9cce433c9744e2e5fe2570
Fuhito Suguri <bitwalk@gmail.com>
2023-10-28 15:39:54 +0900

    update

--- ui_modules/tab_panel_contract.py
+++ ui_modules/tab_panel_contract.py
@@ -1,4 +1,7 @@
+from PySide6.QtWidgets import QGridLayout, QLabel
+
 from ui_modules.panel_abstract import TabPanelAbstract
+from widgets.buttons import ApplyButton
 
 
 class TabPanelContract(TabPanelAbstract):
@@ -6,3 +9,16 @@ class TabPanelContract(TabPanelAbstract):
 
     def __init__(self):
         super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        row = 0
+        # データベースの初期化
+        lab_read = QLabel('取引注文ファイルの読み込み')
+        layout.addWidget(lab_read, row, 0)
+        but_read = ApplyButton()
+        # but_read.clicked.connect()
+        layout.addWidget(but_read, row, 1)
