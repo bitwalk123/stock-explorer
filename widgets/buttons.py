9ddf069e5797a1c58bac78bc73d44b59e945f167
Fuhito Suguri <bitwalk@gmail.com>
2023-10-31 17:55:06 +0900

    update

--- widgets/buttons.py
+++ widgets/buttons.py
@@ -1,6 +1,7 @@
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QPushButton,
-    QRadioButton,
+    QRadioButton, QStyle,
 )
 
 from functions.get_standard_icon import get_standard_icon
@@ -12,6 +13,21 @@ class ApplyButton(QPushButton):
         name_apply = 'SP_DialogApplyButton'
         icon_apply = get_standard_icon(self, name_apply)
         self.setIcon(icon_apply)
+
+
+class ButtonIcon(QPushButton):
+    """Button with Icon
+
+    Args:
+        name_icon(str):name of builtin icon
+    """
+
+    def __init__(self, name_icon: str):
+        super().__init__()
+        pixmap = getattr(QStyle.StandardPixmap, name_icon)
+        icon = QIcon(self.style().standardIcon(pixmap))
+        self.setIcon(icon)
+        self.setContentsMargins(0, 0, 0, 0)
 
 
 class TickerRadioButton(QRadioButton):
