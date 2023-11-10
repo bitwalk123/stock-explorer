2ce8f29cd8f3f918306c13d094174cd330394a9f
Fuhito Suguri <bitwalk@gmail.com>
2023-11-10 19:24:43 +0900

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class StockExplorer(QMainWindow):
     """Main class for this application
     """
     __version__ = '0.1.0'
-    __build__ = '20231107'
+    __build__ = '20231110'
 
     def __init__(self):
         super().__init__()
---
+++ transaction-manager.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableView,
+)
+
+
+class TransactionManager(QMainWindow):
+    """Main class for this application
+    """
+    __version__ = '0.0.1'
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Transaction Manager')
+        self.init_ui()
+
+    def init_ui(self):
+        view = QTableView()
+        self.setCentralWidget(view)
+
+def main():
+    app = QApplication(sys.argv)
+    obj = TransactionManager()
+    obj.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
