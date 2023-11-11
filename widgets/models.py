c1209ef38259c1d5a11f1e3f63879813ec033e2d
Fuhito Suguri <bitwalk@gmail.com>
2023-11-11 09:13:35 +0900

    update

--- widgets/models.py
+++ widgets/models.py
@@ -21,31 +21,12 @@ class TblPredictModel(QAbstractTableModel):
         self._dataframe = dataframe
 
     def rowCount(self, parent=QModelIndex()) -> int:
-        """ Override method from QAbstractTableModel
-
-        Return row count of the pandas DataFrame
-        """
-        if parent == QModelIndex():
-            return len(self._dataframe)
-        return 0
+        return self._dataframe.shape[0]
 
     def columnCount(self, parent=QModelIndex()) -> int:
-        """Override method from QAbstractTableModel
-
-        Return column count of the pandas DataFrame
-        """
-        if parent == QModelIndex():
-            return len(self._dataframe.columns)
-        return 0
+        return self._dataframe.shape[1]
 
     def data(self, index: QModelIndex, role=Qt.ItemDataRole):
-        """Override method from QAbstractTableModel
-
-        Return data cell from the pandas DataFrame
-        """
-        if not index.isValid():
-            return None
-
         row = index.row()
         col = index.column()
         value = self._dataframe.iloc[row, col]
@@ -71,16 +52,11 @@ class TblPredictModel(QAbstractTableModel):
         return None
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: int = ...) -> Any:
-        """Override method from QAbstractTableModel
-
-        Return dataframe index as vertical header data and columns as horizontal header data.
-        """
         if role == Qt.ItemDataRole.DisplayRole:
             if orientation == Qt.Orientation.Horizontal:
                 return str(self._dataframe.columns[section])
 
             if orientation == Qt.Orientation.Vertical:
-                # return str(self._dataframe.index[section])
                 return section + 1
 
         if role == Qt.ItemDataRole.TextAlignmentRole:
