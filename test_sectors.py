a7ae920ea77ce61f198e893577eca4128a40b840
Fuhito Suguri <bitwalk@gmail.com>
2023-12-07 19:28:46 +0900

    update

--- test_sectors.py
+++ test_sectors.py
@@ -10,9 +10,6 @@ if __name__ == '__main__':
     date_report = str(conv_timestamp2date(pair_date[1]))
     print(date_report)
     sector_delta = diff_close_by_sector(pair_date)
-    #for sector in sector_delta.get_sectors():
-        # print(sector, sector_delta.get_dist_raw(sector))
-        #print(sector, sector_delta.get_dist_standardize(sector))
 
     fp_title = FontProperties(fname=r'fonts/RictyDiminished-Regular.ttf', size=16)
     fp_axtitle = FontProperties(fname=r'fonts/RictyDiminished-Regular.ttf', size=14)
@@ -27,8 +24,12 @@ if __name__ == '__main__':
 
     fig = plt.figure(figsize=(8, 10))
     ax = fig.add_subplot(1, 1, 1)
-    ax.boxplot(list_data, labels=list_sector, vert=False,
-               flierprops={'marker': 'o', 'markersize': 2})
+    ax.boxplot(
+        list_data,
+        labels=list_sector,
+        vert=False,
+        flierprops={'marker': 'o', 'markersize': 2}
+    )
 
     for label in ax.get_yticklabels():
         label.set_fontproperties(fp_tick)
@@ -41,4 +42,4 @@ if __name__ == '__main__':
     plt.subplots_adjust(left=0.35, right=0.95, bottom=0.1, top=0.9)
 
     plt.savefig('pool/close_%d.png' % pair_date[1])
-    plt.show()
+    # plt.show()
