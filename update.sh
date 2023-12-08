8c74dba0f36c6cdfdf111a3135f704a3d780e421
Fuhito Suguri <bitwalk@gmail.com>
2023-12-08 10:46:03 +0900

    update

--- update.sh
+++ update.sh
@@ -2,3 +2,9 @@
 cd $(dirname ${0})
 source venv/bin/activate
 python update_ticker_data_db.py
+
+python report_daily_sector.py
+cd /home/bitwalk/MyProjects/stock
+git add report/*/*.png
+git commit -m "update"
+git push
