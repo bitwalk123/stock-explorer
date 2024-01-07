dba271610ac3820d60a4c8fb592decdb000b6d99
Fuhito Suguri <bitwalk@gmail.com>
2024-01-07 17:14:08 +0900

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd $(dirname ${0})
+cd $(dirname ${0}) || exit
 source venv/bin/activate
 python update_ticker_data_db.py
 
 python report_daily_sector.py
-cd /home/bitwalk/MyProjects/stock
+cd /home/bitwalk/MyProjects/stock || exit
 git add report/*/*.png
 git commit -m "update"
 git push
