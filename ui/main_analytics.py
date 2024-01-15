e31741841b0ee50b8cc3abef27eac8e23fd499bd
Fuhito Suguri <bitwalk@gmail.com>
2024-01-15 14:24:27 +0900

    update

--- ui/main_analytics.py
+++ ui/main_analytics.py
@@ -1,3 +1,7 @@
+from PySide6.QtCore import Qt
+
+from ui.dock_navigator import DockNavigator
+from widgets.charts import Trend
 from widgets.tab_panels import TabPanelMain
 
 
@@ -6,3 +10,17 @@ class MainAnalytics(TabPanelMain):
 
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.init_ui()
+
+    def init_ui(self):
+        # CandleStick chart as default
+        chart = Trend()
+        self.setCentralWidget(chart)
+
+        # Bottom Dock
+        self.dock_bottom = dock_bottom = DockNavigator(self, chart)
+        self.addDockWidget(
+            Qt.DockWidgetArea.BottomDockWidgetArea,
+            dock_bottom
+        )
