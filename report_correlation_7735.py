a4bd1aab908c72ae9abbc9c6d349b982028f4455
Fuhito Suguri <bitwalk@gmail.com>
2024-01-22 18:50:28 +0900

    update

---
+++ report_correlation_7735.py
@@ -0,0 +1,17 @@
+import time
+
+from funcs.tide import get_elapsed, get_past_month_day
+from report.corr import correlation_1to1
+from snippets.set_env import set_env
+
+if __name__ == '__main__':
+    dict_info = set_env()
+    time_start = time.time()
+
+    start = get_past_month_day(6)
+    code_target = '7735'
+    file_out = 'pool/corr_7735-%d.pkl'
+
+    correlation_1to1(code_target, start)
+
+    print('elapsed %.3f sec' % get_elapsed(time_start))
