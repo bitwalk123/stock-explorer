624f34bb879b440cf63c7b5f8bba06e9d6e36035
Fuhito Suguri <bitwalk@gmail.com>
2024-03-22 18:25:04 +0900

    update

---
+++ trade_day_analysis.py
@@ -0,0 +1,16 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from ui.main_trade_day_abalysis import MainTradeDayAnalysis
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainTradeDayAnalysis()
+    win.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ ui/main_trade_day_abalysis.py
@@ -0,0 +1,111 @@
+import os
+from typing import Union
+
+import mplfinance as mpf
+
+from PySide6.QtCore import QThreadPool, Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QProgressDialog,
+    QWidget,
+)
+
+from mthreads.get_day_trade import GetDayTradeWorker
+from structs.day_trade import DayTrade
+from structs.res import AppRes
+from ui.dock_navigator import DockNavigator
+from ui.toolbar_trade_day_analysis import ToolBarTradeDayAnalysis
+from widgets.charts import Trend
+from widgets.dialog import DialogAlert
+
+
+class MainTradeDayAnalysis(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.res = AppRes()
+        self.threadpool = QThreadPool()
+
+        self.progress: Union[QProgressDialog, None] = None
+        self.toolbar = None
+        self.dock_bottom = None
+
+        self.setWindowTitle('Day Trade Analysis')
+        self.resize(1200, 700)
+
+        self.toolbar = toolbar = ToolBarTradeDayAnalysis(self)
+        toolbar.drawRequested.connect(self.on_get_day_trade)
+        self.addToolBar(toolbar)
+
+        # CandleStick chart as default
+        chart = Trend(gtype='Trend')
+        self.setCentralWidget(chart)
+
+        # Bottom Dock
+        self.dock_bottom = dock_bottom = DockNavigator(self, chart)
+        self.addDockWidget(
+            Qt.DockWidgetArea.BottomDockWidgetArea,
+            dock_bottom
+        )
+
+    def on_draw_chart(self, info: DayTrade):
+        # Check if dataframe is empty.
+        if len(info.df) == 0:
+            self.no_data_found(info)
+            return
+
+        chart: QWidget | Trend = self.centralWidget()
+        chart.clearAxes()
+        mpf.plot(
+            info.df,
+            type='candle',
+            datetime_format='%H:%M',
+            tight_layout=False,
+            style=self.res.getCandleStyle(),
+            ax=chart.ax
+        )
+        chart.ax.set_title(info.getTitle())
+        chart.ax.set_ylabel('Price')
+        chart.ax.yaxis.set_tick_params(labelright=True)
+        chart.ax.grid()
+        chart.refreshDraw()
+
+        # Hide progress dialog
+        self.progress_hide()
+
+    def on_get_day_trade(self, info: DayTrade):
+        worker = GetDayTradeWorker(info)
+        worker.finished.connect(self.on_draw_chart)
+        self.threadpool.start(worker)
+
+        # Show progress dialog
+        self.progress_show()
+
+    def no_data_found(self, info: DayTrade):
+        # Hide progress dialog
+        self.progress_hide()
+
+        # Warning dialog
+        dlg = DialogAlert()
+        dlg.setText('%s のデータがありませんでした。' % info.start)
+        dlg.exec()
+
+    def progress_hide(self):
+        self.progress.hide()
+        self.progress.deleteLater()
+
+    def progress_show(self):
+        self.progress = progress = QProgressDialog(
+            labelText='Working...',
+            parent=self
+        )
+        icon = QIcon(
+            os.path.join(self.res.getImagePath(), 'hourglass.png')
+        )
+        progress.setWindowIcon(icon)
+        progress.setWindowModality(Qt.WindowModality.WindowModal)
+        progress.setCancelButton(None)
+        progress.setWindowTitle('status')
+        progress.setRange(0, 0)
+        progress.show()
