e8828dd00e1cfee27171a493e88415063979df60
Fuhito Suguri <bitwalk@gmail.com>
2024-04-30 05:25:46 +0900

    update

--- unit_update_table_trade_daily.py
+++ unit_update_table_trade_daily.py
@@ -1,14 +1,19 @@
 import datetime as dt
+import sys
 import time
+
+from PySide6.QtWidgets import QApplication
 
 from funcs.tbl_trade import update_tbl_trade
 from funcs.tide import get_elapsed
 from snippets.set_env import set_env
 
 if __name__ == '__main__':
+    app = QApplication()
     dict_info = set_env()
     time_start = time.time()
     end = dt.date.today()
     result = update_tbl_trade(end)
     print(result)
     print('elapsed %.3f sec' % get_elapsed(time_start))
+    sys.exit(app.exec())
