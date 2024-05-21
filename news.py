fbd7e58ac330fc5c03abdd64b6471b83152d9e87
Fuhito Suguri <bitwalk@gmail.com>
2024-05-21 18:00:29 +0900

    update

--- news.py
+++ news.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtCore import QCoreApplication
+from PySide6.QtCore import QCoreApplication, Signal
 from PySide6.QtWidgets import (
     QApplication,
     QComboBox,
@@ -12,7 +12,9 @@ from PySide6.QtWidgets import (
 from funcs.scraping import get_news_kabuyoho
 
 
-class Example(QMainWindow):
+class ToolBarNews(QToolBar):
+    clickedSearch = Signal(str)
+
     def __init__(self):
         super().__init__()
         self.tickers = tickers = {
@@ -22,27 +24,35 @@ class Example(QMainWindow):
             'レーザーテック': '6920',
             'ディスコ': '6146',
         }
-        self.setWindowTitle('News')
-
-        toolbar = QToolBar()
-        self.addToolBar(toolbar)
 
         self.combo = combo = QComboBox()
         combo.addItems(tickers.keys())
-        toolbar.addWidget(combo)
+        self.addWidget(combo)
 
         but_search = QPushButton('検索')
         but_search.clicked.connect(self.on_search)
-        toolbar.addWidget(but_search)
+        self.addWidget(but_search)
+
+    def on_search(self):
+        key = self.combo.currentText()
+        ticker = self.tickers[key]
+        self.clickedSearch.emit(ticker)
+
+
+class News(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('News')
+
+        toolbar = ToolBarNews()
+        toolbar.clickedSearch.connect(self.on_search)
+        self.addToolBar(toolbar)
 
     def closeEvent(self, event):
         print('アプリケーションを終了します。')
         event.accept()  # let the window close
 
-    def on_search(self):
-        key = self.combo.currentText()
-        ticker = self.tickers[key]
-
+    def on_search(self, ticker: str):
         # 株予想
         results = get_news_kabuyoho(ticker)
         for line in results:
@@ -54,7 +64,7 @@ class Example(QMainWindow):
 
 def main():
     app = QApplication(sys.argv)
-    win = Example()
+    win = News()
     win.show()
     sys.exit(app.exec())
 
