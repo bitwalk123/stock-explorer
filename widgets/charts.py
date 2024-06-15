5e576538ec1d1a4d5748b6418050fbda92982763
Fuhito Suguri <bitwalk@gmail.com>
2024-06-15 10:47:55 +0900

    update

--- widgets/charts.py
+++ widgets/charts.py
@@ -111,9 +111,9 @@ class ChartForAnalysis(ChartAbstract):
             right=0.995,
             hspace=0,
         )
-        self.ax1 = self.fig.add_subplot(311)  # 3x1の1つめ
-        self.ax2 = self.fig.add_subplot(312)  # 3x2の2つめ
-        self.ax3 = self.fig.add_subplot(313)  # 3x3の3つめ
+        self.ax1 = self.fig.add_subplot(4, 1, (1, 2))  # 3x1の1つめ
+        self.ax2 = self.fig.add_subplot(4, 1, 3)  # 3x2の2つめ
+        self.ax3 = self.fig.add_subplot(4, 1, 4)  # 3x3の3つめ
 
     def clearAxes(self):
         self.ax1.cla()
