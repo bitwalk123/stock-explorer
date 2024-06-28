e1ce728abed56044faf7122c10fd2ceead79de7d
Fuhito Suguri <bitwalk@gmail.com>
2024-06-29 06:33:40 +0900

    update

--- unit_create_table_trade1m.py
+++ unit_create_table_trade1m.py
@@ -6,7 +6,9 @@ from funcs.tbl_trade_day import create_tbl_trade1m
 from snippets.set_env import set_env
 
 if __name__ == '__main__':
+    # This is needed for PySide6 > 6.7
     app = QApplication(sys.argv)
+
     dict_info = set_env()
 
     # create trade table
