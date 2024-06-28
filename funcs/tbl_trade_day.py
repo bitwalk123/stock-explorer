4178880e482aa5b83319091066b46d5443bc61a6
Fuhito Suguri <bitwalk@gmail.com>
2024-06-29 06:28:46 +0900

    update

--- funcs/tbl_trade_day.py
+++ funcs/tbl_trade_day.py
@@ -2,11 +2,12 @@ from PySide6.QtSql import QSqlQuery
 
 from sqls.sql_trade_day import (
     sql_create_tbl_trade5m,
-    sql_drop_tbl_trade5m,
+    sql_drop_tbl_trade5m, sql_create_tbl_trade1m, sql_drop_tbl_trade1m,
 )
 from structs.db_info import DBInfo
 
-
+# _____________________________________________________________________________
+# for 5 minutes chart
 def create_tbl_trade5m():
     con = DBInfo.get_connection()
 
@@ -41,3 +42,37 @@ def drop_tbl_trade5m() -> bool:
         return False
 
 
+# _____________________________________________________________________________
+# for 1 minute chart
+def create_tbl_trade1m():
+    con = DBInfo.get_connection()
+
+    if con.open():
+        print('connected!')
+        create_tbl_trade1m_procs()
+        con.close()
+        return True
+    else:
+        print('database can not be opened!')
+        return False
+
+
+def create_tbl_trade1m_procs():
+    query = QSqlQuery()
+    sql = sql_create_tbl_trade1m()
+    if not query.exec(sql):
+        print(query.lastError())
+
+
+def drop_tbl_trade1m() -> bool:
+    con = DBInfo.get_connection()
+
+    if con.open():
+        query = QSqlQuery()
+        sql = sql_drop_tbl_trade1m()
+        result = query.exec(sql)
+        con.close()
+        return result
+    else:
+        print('database can not be opened!')
+        return False
---
+++ unit_create_table_trade1m.py
@@ -0,0 +1,14 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from funcs.tbl_trade_day import create_tbl_trade1m
+from snippets.set_env import set_env
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dict_info = set_env()
+
+    # create trade table
+    result = create_tbl_trade1m()
+    print(result)
