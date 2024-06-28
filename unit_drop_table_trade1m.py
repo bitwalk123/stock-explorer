9fef1eca1b6aa5e2dcaec734fa5f185b95161644
Fuhito Suguri <bitwalk@gmail.com>
2024-06-29 06:41:02 +0900

    update

---
+++ unit_drop_table_trade1m.py
@@ -0,0 +1,14 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from funcs.tbl_trade_day import drop_tbl_trade1m
+from snippets.set_env import set_env
+
+if __name__ == '__main__':
+    # This is needed for PySide6 > 6.7
+    app = QApplication(sys.argv)
+
+    dict_info = set_env()
+    result = drop_tbl_trade1m()
+    print(result)
