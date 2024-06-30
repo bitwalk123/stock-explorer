a56548d0e61b87ce2a28fef1b244f0ad135e3bab
Fuhito Suguri <bitwalk@gmail.com>
2024-06-30 18:15:48 +0900

    update

--- ui/dock_dta.py
+++ ui/dock_dta.py
@@ -5,6 +5,8 @@ from PySide6.QtWidgets import QDockWidget, QSlider
 class DTADockSlider(QDockWidget):
     def __init__(self):
         super().__init__()
+        self.setFeatures(QDockWidget.DockWidgetFeature.NoDockWidgetFeatures)
+
         slider = QSlider()
         slider.setOrientation(Qt.Orientation.Horizontal)
         self.setWidget(slider)
