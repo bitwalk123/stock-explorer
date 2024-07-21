869f1407530be09586255cf7bae82ff6732305cc
Fuhito Suguri <bitwalk@gmail.com>
2024-07-22 08:49:32 +0900

    update

---
+++ mktreview.py
@@ -0,0 +1,181 @@
+import datetime as dt
+import sys
+import yfinance as yf
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow, QScrollArea, QWidget, QGridLayout, QLabel, QFrame,
+)
+
+
+class MktReview(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Market Review')
+
+        area = QScrollArea()
+        area.setWidgetResizable(True)
+        self.setCentralWidget(area)
+
+        base = QWidget()
+        area.setWidget(base)
+
+        layout = QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.setAlignment(
+            Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft
+        )
+        base.setLayout(layout)
+
+        date_end = dt.date.today()
+        date_start = date_end + dt.timedelta(days=-14)
+
+        row = 0
+        lab_title = QLabel('銘柄・指数')
+        lab_title.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Raised)
+        lab_title.setLineWidth(1)
+        lab_title.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_title, row, 0)
+
+        lab_date = QLabel('取引日')
+        lab_date.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Raised)
+        lab_date.setLineWidth(1)
+        lab_date.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_date, row, 1)
+
+        lab_close = QLabel('終値')
+        lab_close.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Raised)
+        lab_close.setLineWidth(1)
+        lab_close.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_close, row, 2)
+
+        lab_delta = QLabel('前日差')
+        lab_delta.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Raised)
+        lab_delta.setLineWidth(1)
+        lab_delta.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_delta, row, 3)
+
+        row += 1
+        ticker = '^DJI'
+        name_ticker = 'ダウ工業株30種平均'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = '^IXIC'
+        name_ticker = 'ナスダック総合指数'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = '^GSPC'
+        name_ticker = 'S&P500'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = '^SOX'
+        name_ticker = 'PHLX Semiconductor'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'NVDA'
+        name_ticker = 'NVIDIA'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'AAPL'
+        name_ticker = 'Apple'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'ASML'
+        name_ticker = 'ASML Holding N.V.'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'TSM'
+        name_ticker = 'TSMC'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'ARM'
+        name_ticker = 'ARM Holdings PLC'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'MU'
+        name_ticker = 'Micron'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'INTC'
+        name_ticker = 'Intel'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'AMAT'
+        name_ticker = 'Applied Materials'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'LRCX'
+        name_ticker = 'Lam Research'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+        row += 1
+        ticker = 'TOELY'
+        name_ticker = 'Tokyo Electron Ltd. (ADR)'
+        self.add_row(layout, row, date_start, date_end, ticker, name_ticker)
+
+    def add_row(self, layout, row, date_start, date_end, ticker, name_ticker):
+        date_prev, price_close, delta, bcolor = self.get_latest(ticker, date_start, date_end)
+
+        lab_title = QLabel(name_ticker)
+        lab_title.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
+        lab_title.setLineWidth(1)
+        lab_title.setStyleSheet('QLabel{background-color: %s}' % bcolor)
+        layout.addWidget(lab_title, row, 0)
+
+        lab_date = QLabel(date_prev)
+        lab_date.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
+        lab_date.setLineWidth(1)
+        lab_date.setStyleSheet('QLabel{font-family: monospace; background-color: %s}' % bcolor)
+        layout.addWidget(lab_date, row, 1)
+
+        lab_close = QLabel(price_close)
+        lab_close.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
+        lab_close.setLineWidth(1)
+        lab_close.setStyleSheet('QLabel{font-family: monospace; background-color: %s}' % bcolor)
+        lab_close.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_close, row, 2)
+
+        lab_delta = QLabel(delta)
+        lab_delta.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
+        lab_delta.setLineWidth(1)
+        lab_delta.setStyleSheet('QLabel{font-family: monospace; background-color: %s}' % bcolor)
+        lab_delta.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        layout.addWidget(lab_delta, row, 3)
+
+    def get_latest(self, ticker, date_start, date_end):
+        df = yf.download(ticker, date_start, date_end)
+        date_prev = str(df.tail(1).index[0].date())
+        price_close = '%.2f' % df.tail(2)['Adj Close'].iloc[1]
+        delta = df.tail(2)['Adj Close'].iloc[1] - df.tail(2)['Adj Close'].iloc[0]
+        delta_str = '%.2f' % delta
+        if delta < 0:
+            bcolor = '#efe'
+        else:
+            bcolor = '#fee'
+
+        return date_prev, price_close, delta_str, bcolor
+
+
+def main():
+    app = QApplication()
+    ex = MktReview()
+    ex.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
