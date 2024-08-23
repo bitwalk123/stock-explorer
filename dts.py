eba0d9571936503d4c4dd4dbca251e1b6dfe9e05
Fuhito Suguri <bitwalk@gmail.com>
2024-08-24 08:23:48 +0900

    update

--- dts.py
+++ dts.py
@@ -1,20 +1,27 @@
 import os
+import numpy as np
 import pandas as pd
 import sys
 
-from PySide6.QtCore import Qt, QDate
+from PySide6.QtCore import Qt, QDate, QDir
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QApplication,
+    QFileDialog,
     QMainWindow,
 )
 
-from funcs.dta_funcs import dta_get_data_from_dbrt
-from funcs.tbl_ticker import get_dict_id_code
-from funcs.tide import get_day_timestamp
 from snippets.set_env import set_env
 from structs.res import AppRes
 from ui.toolbar_dts import DTSToolBar
+
+
+class MyTrade:
+    def __init__(self):
+        self.result = 0
+
+    def update(self, t: pd.Timestamp, price: np.float64):
+        print(t, price)
 
 
 class DayTrendSimulator(QMainWindow):
@@ -31,11 +38,21 @@ class DayTrendSimulator(QMainWindow):
         # _____________________________________________________________________
         # Toolbar
         self.toolbar = toolbar = DTSToolBar()
-        self.toolbar.folderClicked.connect(self.open_file)
+        self.toolbar.folderClicked.connect(self.on_open)
         self.addToolBar(Qt.ToolBarArea.TopToolBarArea, toolbar)
 
-    def open_file(self):
-        print('DEBUG!')
+    def on_open(self, QStringList=None):
+        dialog = QFileDialog()
+        if dialog.exec():
+            filename = dialog.selectedFiles()[0]
+            df = pd.read_pickle(filename)
+            self.simulation(df)
+
+    def simulation(self, df: pd.DataFrame):
+        trade = MyTrade()
+        for t in df.index:
+            price = df.loc[t]['Price']
+            trade.update(t, price)
 
 
 def main():
