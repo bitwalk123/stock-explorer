741ca83aaaa93b6ad933c81944f02fa4eed1e5bc
Fuhito Suguri <bitwalk@gmail.com>
2024-08-24 19:28:21 +0900

    update

--- trade/auto_trade_01.py
+++ trade/auto_trade_01.py
@@ -7,7 +7,8 @@ from trade.auto_trade import AutoTrade
 
 class AutoTrade01(AutoTrade):
     """
-    成行売買で、呼値で売却・買戻
+    成行売買で、呼値 (tick) で売却・買戻
+    損切り (Loss-cut) 条件無し
     """
     def __init__(self, t: pd.Timestamp):
         super().__init__(t)
@@ -17,6 +18,7 @@ class AutoTrade01(AutoTrade):
             self.status = TradeStatus.HOLD
             self.t0 = t
             self.price0 = price
+            return
 
         delta = self.calcDelta(t, price)
 
@@ -27,6 +29,31 @@ class AutoTrade01(AutoTrade):
 
         if self.status == TradeStatus.HOLD:
             if delta > 0:
+                print(t, 'buy', self.price_own, self.price_limit, price, delta, self.result)
                 self.buy(t, price)
+                print(t, 'bought', self.price_own, self.price_limit, price, delta, self.result)
             elif delta < 0:
+                print(t, 'sell', self.price_own, self.price_limit, price, delta, self.result)
                 self.sell(t, price)
+                print(t, 'sold', self.price_own, self.price_limit, price, delta, self.result)
+            else:
+                print(t, 'hold/stay', self.price_own, self.price_limit, price, delta, self.result)
+            return
+
+        if self.status == TradeStatus.BOUGHT:
+            if self.price_limit < price:
+                print(t, 'transaction/match', self.price_own, self.price_limit, price, delta, self.result)
+                self.transaction(t, price)
+                print(t, 'transaction/end', self.price_own, self.price_limit, price, delta, self.result)
+            else:
+                print(t, 'bought/stay', self.price_own, self.price_limit, price, delta, self.result)
+            return
+
+        if self.status == TradeStatus.SOLD:
+            if price < self.price_limit:
+                print(t, 'transaction/match', self.price_own, self.price_limit, price, delta, self.result)
+                self.transaction(t, price)
+                print(t, 'transaction/end', self.price_own, self.price_limit, price, delta, self.result)
+            else:
+                print(t, 'sold/stay', self.price_own, self.price_limit, price, delta, self.result)
+            return
