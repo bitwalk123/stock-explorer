2fe70df433a43ba3ddcb04fb698db4b47f8804df
Fuhito Suguri <bitwalk@gmail.com>
2024-08-26 05:20:58 +0900

    update

--- widgets/tables.py
+++ widgets/tables.py
@@ -1,4 +1,7 @@
-from PySide6.QtWidgets import QTableView, QHeaderView
+from typing import Union
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QTableView, QHeaderView, QAbstractButton, QVBoxLayout, QLabel
 
 
 class TableView(QTableView):
@@ -18,3 +21,38 @@ class TableView(QTableView):
 
     def getLabel(self) -> str:
         return self.label
+
+def add_label(but: QAbstractButton):
+    layout = QVBoxLayout()
+    layout.setContentsMargins(0, 0, 0, 0)
+    but.setLayout(layout)
+    label = QLabel()
+    label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    layout.addWidget(label)
+    return label
+
+
+class PandasTableView(QTableView):
+    __version__ = '0.0.1'
+
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("""
+            QTableView{
+                font-family: monospace;
+            }
+            QTableCornerButton::section{
+                border-width: 1px;
+                border-color: #BABABA;
+                border-style: outset;
+            }
+        """)
+        but: Union[object, QAbstractButton] = self.findChild(QAbstractButton, '')
+        if type(but) is QAbstractButton:
+            self.label = add_label(but)
+        else:
+            self.label = None
+
+    def setUpperLeftCornerTitle(self, title: str):
+        if self.label is not None:
+            self.label.setText(title)
