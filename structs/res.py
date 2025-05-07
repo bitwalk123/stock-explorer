8ed21f076454f5a765317fbc4e0485955849e199
Fuhito Suguri <bitwalk@gmail.com>
2025-05-07 13:57:08 +0900

    update

--- structs/res.py
+++ structs/res.py
@@ -1,2 +1,10 @@
+from PySide6.QtWidgets import QStyle, QWidget
+
+
 class AppRes:
+    dir_font = 'fonts'
     dir_image = 'images'
+
+    def getBuiltinIcon(self, parent: QWidget, name: str):
+        pixmap_icon = getattr(QStyle.StandardPixmap, 'SP_%s' % name)
+        return parent.style().standardIcon(pixmap_icon)
