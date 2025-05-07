801d9cbda060f2bc4fe6719324e8edc9241c5416
Fuhito Suguri <bitwalk@gmail.com>
2025-05-07 10:11:15 +0900

    update

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
+import os
 import sys
 
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QApplication,
     QTabWidget,
 )
 
+from structs.res import AppRes
+
 
 class StockExplorer(QTabWidget):
+    __app_name__ = 'Stock Explorer'
     __version__ = '0.4.0'
 
     def __init__(self):
         super().__init__()
+        self.res = res = AppRes()
+
+        icon = QIcon(os.path.join(res.dir_image, 'stock.png'))
+        self.setWindowIcon(icon)
+        self.setWindowTitle(self.__app_name__)
 
 
 def main():
