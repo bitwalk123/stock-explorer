ecd92ac30615d062bc5869d683f2d1f6b946cf77
Keiichi Takahashi <bitwalk@gmail.com>
2025-05-18 08:33:39 +0900

    update

--- sample_001_xlwings.py
+++ sample_001_xlwings.py
@@ -8,22 +8,25 @@ if __name__ == '__main__':
     sheet = wb.sheets['Sheet1']
     col_code = 0
     col_name = 1
-    col_time = 2
-    col_price = 3
+    col_date = 2
+    col_time = 3
+    col_price = 4
 
     r = 0
     print(sheet[r, col_code].value)
     print(sheet[r, col_name].value)
+    print(sheet[r, col_date].value)
     print(sheet[r, col_time].value)
     print(sheet[r, col_price].value)
 
     while True:
         r += 1
         val_code = sheet[r, col_code].value
+        val_price = sheet[r, col_price].value
         if val_code == terminator:
             break
         else:
-            print(val_code)
+            print(val_code, val_price)
 
     n = r
     print('terminator row', n)
