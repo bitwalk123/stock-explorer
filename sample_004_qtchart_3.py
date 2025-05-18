1a0f43c6037675a71302a4fb18a1a4b7a0edc93d
Keiichi Takahashi <bitwalk@gmail.com>
2025-05-19 06:58:28 +0900

    update

--- sample_004_qtchart_3.py
+++ sample_004_qtchart_3.py
@@ -1,3 +1,5 @@
+# 楽天証券マーケットスピードⅡ RSS で Excel 上に取得した株価データを
+# Python で読み込み株価トレンドをリアルタイムに描画するサンプル
 import sys
 
 import pandas as pd
@@ -12,7 +14,7 @@ from widgets.views import TickView
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.res = res = AppRes()
+        self.res = AppRes()
 
         self.df = pd.DataFrame()
         self.row = 0
@@ -20,10 +22,12 @@ class Example(QMainWindow):
         self.msec_delta = 9 * 60 * 60 * 1000
         self.resize(1000, 500)
 
+        # 情報を取得する Excel ファイル
         name_excel = 'daytrader.xlsx'
         wb = xw.Book(name_excel)
         self.sheet = wb.sheets['Sheet1']
 
+        # 列情報
         self.col_code = 0
         self.col_name = 1
         self.col_date = 2
@@ -31,13 +35,26 @@ class Example(QMainWindow):
         self.col_price = 4
         self.col_price_prev = 5
 
+        # 時差と市場時間
         self.msec_delta = 9 * 60 * 60 * 1000
         self.t_start = QTime(9, 0, 0)
         self.t_end = QTime(15, 30, 0)
 
+        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+        # ユーザー・インターフェイス
+        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+
+        """
+        チャート用インスタンス
+        リアルタイムでプロットを更新するには、
+        Matplotlib より QChart を利用した方が簡単にできる。
+        """
         self.view = view = TickView()
         self.setCentralWidget(view)
 
+        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+        # タイマー
+        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
         self.timer = timer = QTimer()
         timer.timeout.connect(self.on_update_data)
         timer.setInterval(1000)
@@ -48,6 +65,7 @@ class Example(QMainWindow):
         p_current = self.sheet[1, self.col_price].value
 
         if self.t_start <= t_current <= self.t_end:
+            # msec の数値へ変換すると UTC になってしまうため時差を調整
             x = t_current.msecsSinceStartOfDay() - self.msec_delta
             y = float(p_current)
             self.view.appendPoint(x, y)
