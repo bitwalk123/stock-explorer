e13618683b51ce8370f3f009944e18ca9c98d9d8
Fuhito Suguri <bitwalk@gmail.com>
2025-05-24 09:36:39 +0900

    update

--- widgets/docks.py
+++ widgets/docks.py
@@ -37,42 +37,53 @@ class DockTrader(QDockWidget):
 
         base.setLayout(layout)
 
+        # 現在株価表示
         self.lcd_price = lcd_price = LCDNumber(self)
         layout.addWidget(lcd_price)
 
+        # 売買用ボタンの行
         row_buysell = Widget()
         layout.addWidget(row_buysell)
 
         layout_buysell = HBoxLayout()
         row_buysell.setLayout(layout_buysell)
 
+        # 売掛ボタン
         but_sell = ButtonSell()
         layout_buysell.addWidget(but_sell)
 
+        # 余白
         pad = PadH()
         layout_buysell.addWidget(pad)
 
+        # 買掛ボタン
         but_buy = ButtonBuy()
         layout_buysell.addWidget(but_buy)
 
+        # 含み損益表示
         self.lcd_profit = lcd_profit = LCDNumber(self)
         layout.addWidget(lcd_profit)
 
+        # 建玉返済ボタン
         but_repay = ButtonRepay()
         layout.addWidget(but_repay)
 
+        # 合計損益表示
         self.lcd_total = lcd_total = LCDNumber(self)
         layout.addWidget(lcd_total)
 
+        # その他ツール用フレーム
         row_tool = Frame()
         layout.addWidget(row_tool)
 
         layout_tool = HBoxLayout()
         row_tool.setLayout(layout_tool)
 
+        # 余白
         pad = PadH()
         layout_tool.addWidget(pad)
 
+        # 画像保存ボタン
         but_save = ButtonSave()
         but_save.clicked.connect(self.on_save)
         layout_tool.addWidget(but_save)
